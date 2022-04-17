7b74a93a1340311b1b27eb84cff415b55e5344a0
Cooper Myers <60561014+cooperalanmyers@users.noreply.github.com>
2022-04-17 14:49:14 -0400

    Update CentralizedServer.py

--- CentralizedServer.py
+++ CentralizedServer.py
@@ -8,26 +8,20 @@ import socket
 # Threading Examples:
 # https://www.dev2qa.com/how-to-use-python-thread-example
 
-
+# Creating Central Server
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 serverAddr = ('localhost', '2309')
 socket.bind(serverAddr)
 socket.listen()
 
-
 # Users Table
-
 userName = []
 connectionSpeed = []
 hostName = []
 
 # Other Info
-
 connections = [] # Used For Holding Connections
 userList = []
-
-
 currentUsers = []
 allUsers = [] # Holding User Information
 userFileDescriptions = []
@@ -39,9 +33,9 @@ def clientThread(connectionSocket):
     try:
         
         client = connectionSocket.recv(1024).decode() # Recieving Client Name
-        client = client.split(" ") # Splitting Names by Spaces
+        client = client.split(" ") # Splitting Name by Spaces
 
-        connections.append(connectionSocket) # Appending Socket Connetion to Array
+        connections.append(connectionSocket) # Appending Socket Connection to Array
         userList.append(client[0]) # Appending Client to Array
         userList.append(client[1]) # Appending Client to Array
         userList.append(client[2]) # Appending Client to Array
